23e09913f72adf7f7d481cfd8b6ef35a3cb806fd
Xiaoming Cao <cax@ad.unc.edu>
2024-09-09 11:49:42 -0400

    added comment.

--- exercises/ex01_tea_party.py
+++ exercises/ex01_tea_party.py
@@ -1,22 +1,17 @@
-__author__: str = "730753482"
-
-
 """
 A function to compute the number of tea bags needed based on number of guests
-A function to compute the number of treats needed based on the number of teas guests 
-are expecting to drink
 A function to compute the cost of the tea bags and the treats combined
-Bringing these function together in a “main planner” function that calls each and 
-produces printed output
 Making the program runnable and asking a user for input when they run the program
 """
+
+__author__: str = "730753482"
 
 
 def tea_bags(people: int) -> int:
     """
     Calculate the number of teabags needed.
     """
-    return people * 2
+    return people * 2 #return the total number of teabags.
 
 
 def treats(people: int) -> int:
@@ -24,22 +19,26 @@ def treats(people: int) -> int:
     Calculates the number of treats needed based on the number of teas guests are
     expecting to drink.
     """
-    return int(tea_bags(people=people) * 1.5)
+    return int(tea_bags(people=people) * 1.5) # returns the total number of treats using the number of people.
 
 
 def cost(tea_count: int, treat_count: int) -> float:
     """
     Calculates the total cost of the tea party.
     """
-    return 0.5 * tea_count + 0.75 * tea_count
+    return 0.5 * tea_count + 0.75 * treat_count #return the total costs of the tea and treats by multiplying number of items with their corresponding costs.
 
 
 def main_planner(guests: int) -> None:
-    print("A Cozy Tea Party for " + str(guests) + " People!")
-    print("Tea Bags:" + str(tea_bags(people=guests)))
-    print("Treats:" + str(treats(guests)))
-    print("Cost:$" + str(cost(tea_bags(guests), treats(guests))))
+    """
+    Print the statements
+    """
+    print("A Cozy Tea Party for " + str(guests) + " People!") #print the number of guests.
+    print("Tea Bags: " + str(tea_bags(people=guests)))#print the number of tea bags.
+    print("Treats: " + str(treats(guests)))#print the number of treats.
+    print("Cost: $" + str(cost(tea_bags(guests), treats(guests))))#prints the total costs of the tea party.
+
 
 
 if __name__ == "__main__":
-    main_planner(guests=int(input("How many guests are attending your tea party? ")))
+    main_planner(guests=int(input("How many guests are attending your tea party? "))) #run the main method.
