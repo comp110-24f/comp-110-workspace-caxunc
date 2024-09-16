80071c2b6f1cafa2fcda70c974a26bcb05ab45cc
Xiaoming Cao <caoxiaoming0930@gmail.com>
2024-09-16 11:58:04 -0400

    finished ex

--- CQs/cq02_conditionals.py
+++ CQs/cq02_conditionals.py
@@ -11,20 +11,18 @@ def guess_a_number() -> None:
     A function which takes no input and does what is said in the docstring.
     """
     secret: int = 7  # declare a secret number and it is equal to 7
-    guessed: str = input("Guess a number: ")  # get a user input
-    print("your guess was " + guessed)  # print out the user input
+    user_guess: str = input("Guess a number:")  # get a user input
+    print("your guess was " + user_guess)  # print out the user input
 
-    if int(guessed) == 7:  # if the guessed value equals to 7.
+    if int(user_guess) == secret:  # if the guessed value equals to 7.
         print("You got it!")
 
-    elif int(guessed) < 7:  # if the guessed value is less than 7.
+    if int(user_guess) < secret:  # if the guessed value is less than 7.
         print("Your guess was too low! The secret number is " + str(secret))
 
-    elif int(guessed) > 7:  # if the guessed value is greater than 7
+    if int(user_guess) > secret:  # if the guessed value is greater than 7
         print("Your guess was too high! The secret number is " + str(secret))
 
-    return None
 
-
-if __name__ == "main":
+if __name__ == "__main__":
     guess_a_number()  # execute guess the number method.
---
+++ exercises/ex02_chardle.py
@@ -0,0 +1,68 @@
+"""EX02 - Charcle - A cute step toward Wordle."""
+
+___author___ = "730753482"
+
+
+def input_word() -> str: 
+    """
+    This is a funciton that would get a five letter word from the user, and returns 
+    an error if the input is not.
+    """
+    word: str = input("Enter a 5-character word: ")  # Get input from user.
+
+    if (len(word) < 5 or len(word) > 5):
+        print("Error: Word must contain 5 characters.")
+        exit()  # Test for the length of the word, and exits if it is not five. 
+    return word
+
+
+def input_letter() -> str:
+    """
+    This is a function that gets a letter fromt eh user, and returns an error if 
+    the input is not.
+    """
+    character: str = input("Enter a single character:")  # Gets input from user.
+
+    if (len(character) > 1):
+        print("Error: Character must be a single character.")
+        exit()  # Test fro the length of the word, and exists if it is not.
+    return character
+
+
+def contains_char(word: str, letter: str) -> None:
+    """
+    This is a function which checks whther the letter chosen is within the word given, 
+    and if the letter is in the word, it would return it's index.
+    """
+    count = 0  # keep counts of all number of instances which the letter appears. 
+    if word[0] == letter:
+        print(letter + " found at index " + str(0))
+        count += 1
+    if word[1] == letter:
+        print(letter + " found at index " + str(1))
+        count += 1
+    if word[2] == letter:
+        print(letter + " found at index " + str(2))
+        count += 1
+    if word[3] == letter:
+        print(letter + " found at index " + str(3))
+        count += 1
+    if word[4] == letter:
+        print(letter + " found at index " + str(4))
+        count += 1
+
+    if count == 0:  # returns "no instances" found if count is 0.
+        print("No instances of" + letter + "found in " + word)
+    elif count == 1:
+        print('1' + " instance of " + letter + " found in " + word)
+    else:
+        print(str(count) + " instances of " + letter + " found in " + word)
+    
+
+def main():
+    contains_char(word=input_word(), letter=input_letter())
+
+
+if __name__ == "main":
+    main()
+    
